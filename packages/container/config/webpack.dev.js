13eaf00cd87e64e4abbca34406cddabb5bafac3a
evroka <nata_safonova@inbox.ru>
2021-10-26 20:35:15 +0400

    Added AuthApp

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -3,9 +3,11 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJSON = require('../package.json');
 
-
 const devConfig = {
 	mode: 'development',
+	output: {
+		publicPath: 'http://localhost:8080/'
+	},
 	devServer: {
 		port: 8080,
 		historyApiFallback: {
@@ -17,6 +19,7 @@ const devConfig = {
 			name: 'container',
 			remotes: {
 				marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+				auth: 'auth@http://localhost:8082/remoteEntry.js',
 			},
 			shared: packageJSON.dependencies,
 		}),
